21ff700de7b9d4b8ebb54aa88849e35ac8c21e7e
hanjh <947827078@qq.com>
2018-05-15 09:06:55 +0800

    实现了全节点的钱包和二维码功能

--- Server/cryptor.py
+++ Server/cryptor.py
@@ -5,6 +5,8 @@ import hashlib
 from base58 import *
 import base64
 
+import codeCreate
+
 def createKey(name):
     (publickey, privatekey) = rsa.newkeys(1024)
     if os.path.exists("../KeyFile/"+name):
@@ -15,18 +17,14 @@ def createKey(name):
     timeStr = str(time.time())
     pubName = "pub_"+name+".pem"
     privName = "priv_"+name+".pem"
+    addName = "add_"+name+".addr"
     with open(dirName+"/"+pubName,"w+") as pubF:
         pubF.write(publickey.save_pkcs1().decode())
+        pubF.close()
     with open(dirName+"/"+privName,"w+") as privF:
         privF.write(privatekey.save_pkcs1().decode())
-    pass
-
-#to address
-
-def getPayAddr(name):
-    with open('../KeyFile/'+name+'/pub_'+name+'.pem', 'r') as f:
-        publickey = rsa.PublicKey.load_pkcs1(f.read().encode())
-    #使用两层sha256对公钥进行hash运算
+        privF.close()
+    #create AnonyPay address
     hash_256 = hashlib.sha3_256()
     hash_256.update(str(publickey).encode("utf-8"))
     hash_256_value = hash_256.hexdigest()
@@ -35,10 +33,21 @@ def getPayAddr(name):
     hash_256_2.update(str(hash_256_value).encode('utf-8'))
     hash_256_2_value = hash_256_2.hexdigest()
 
-    #使用RIPEMD160对上一次结果进行运算
-    rip_160 = hashlib.new("ripemd160",hash_256_2_value.encode('utf-8'))
+    # 使用RIPEMD160对上一次结果进行运算
+    rip_160 = hashlib.new("ripemd160", hash_256_2_value.encode('utf-8'))
     rip_160_value = rip_160.hexdigest()
-    return rip_160_value
+    codeCreate.getCountAddress(name, rip_160_value)
+    with open(dirName + "/" + addName, "w+") as addF:
+        addF.write(rip_160_value)
+        addF.close()
+    pass
+
+#to address
+
+def getPayAddr(name):
+    with open('../KeyFile/'+name+'/add_'+name+'.addr', 'r') as f:
+        addr = f.readline()
+    return addr
 
 def signByPriv(name,data):
     with open('../KeyFile/'+name+'/priv_'+name+'.pem', 'r') as f:
@@ -85,6 +94,6 @@ def verifyByPub(name,message,signature):
 # result = verifyByPub("AnonyPay",data2,sign)
 #
 # print(result)
-
+# createKey("hjh")
 # print(getPayAddr("hjh"))
 # print(getPayAddr("ljp"))
